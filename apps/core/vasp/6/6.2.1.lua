817fd2a517e0c0c77a292067270732e71d467a30
Andy Turner <a.turner@epcc.ed.ac.uk>
2021-10-03 10:12:20 +0100

    Adds note on SAFE access

--- apps/core/vasp/6/6.2.1.lua
+++ apps/core/vasp/6/6.2.1.lua
@@ -16,6 +16,11 @@ If you are only interested in the gamma-point, the
 gamma-point version of the code runs about 30-50%
 faster than the complex code.
 
+For access to ONETEP, please request access through
+SAFE. see:
+
+  * https://epcced.github.io/safe-docs/safe-for-users/#how-to-request-access-to-a-package-group
+
    Compiled: 1 October 2021"
    Maintained by: A. R. Turner, EPCC"
 ]])
