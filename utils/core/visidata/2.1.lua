bcd66ff66ac78e67c9fcbdd94bee60906ac7e991
Andy Turner <a.turner@epcc.ed.ac.uk>
2021-10-03 10:26:46 +0100

    Adds initial visidata module

---
+++ utils/core/visidata/2.1.lua
@@ -0,0 +1,23 @@
+help([[
+VisiData
+========
+
+VisiData is an interactive multitool for tabular data.
+It combines the clarity of a spreadsheet, the efficiency
+of the terminal, and the power of Python, into a
+lightweight utility which can handle millions of rows with ease.
+
+  Installed by: Andy Turner, EPCC
+  Date: 4 October 2020
+]])
+
+always_load("cray-python")
+
+local pkgNameVer = myModuleFullName()
+local pkgVersionBase = pathJoin("/work/y07/shared/utils/core", pkgNameVer)
+
+prepend_path("PATH", pathJoin(pkgVersionBase, "bin"))
+prepend_path("PYTHONPATH", pathJoin(pkgVersionBase, "lib/python3.8/site-packages"))
+prepend_path("MANPATH", pathJoin(pkgVersionBase, "lib/python3.8/site-packages"))
+
+family("visidata")
