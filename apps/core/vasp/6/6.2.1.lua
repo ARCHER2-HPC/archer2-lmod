ebb0c28cdd10c5272e0cf6e6560b604790fb1c8f
Andy Turner <a.turner@epcc.ed.ac.uk>
2021-10-03 09:55:29 +0100

    Initial versions of vasp modules

---
+++ apps/core/vasp/6/6.2.1.lua
@@ -0,0 +1,34 @@
+help([[
+VASP 6.2.1
+==========
+
+This module sets up your environment to
+access VASP 6.2.1
+
+Once this module has been loaded you can
+access the VASP 6.2.1 executables as:
+
+ *'vasp_std'       - Multiple k-point (complex) version
+ *'vasp_gam'       - Gamma-point only version
+ *'vasp_ncl'       - Multiple k-point, noncollinear version
+
+If you are only interested in the gamma-point, the
+gamma-point version of the code runs about 30-50%
+faster than the complex code.
+
+   Compiled: 1 October 2021"
+   Maintained by: A. R. Turner, EPCC"
+]])
+
+local pkgName = myModuleName()
+local pkgNameVer = myModuleFullName()
+local pkgNameBase = pathJoin("/work/y07/shared/apps/core", pkgName)
+local pkgVersionBase = pathJoin("/work/y07/shared/apps/core", pkgNameVer)
+
+prepend_path("PATH", pathJoin(pkgVersionBase, "bin"))
+setenv("VASP", pkgVersionBase)
+
+setenv("VASP_VDW_KERNEL", pathJoin(pkgNameBase, "vdw_kernel"))
+setenv("VASP_PSPOT_DIR", pathJoin(pkgNameBase, "potpaw"))
+
+family("vasp")
