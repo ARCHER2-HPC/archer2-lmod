7fc1d89279c23b239957a241d4ac2e5db3deeeab
ARCHER2 CSE Service <support@archer2.ac.uk>
2021-10-13 15:31:11 +0100

    Added NAMD modules

---
+++ apps/core/namd/2.14-smp.lua
@@ -0,0 +1,31 @@
+help([[
+NAMD 2.14.smp
+=============
+NAMD is a parallel molecular dynamics code designed for high-performance simulation of large biomolecular systems,
+see https://www.ks.uiuc.edu/Research/namd/ .
+
+This NAMD 2.14 has been built with SMP support, which introduces a dedicated communication thread for every process. 
+
+This module sets up your environment to access NAMD 2.14 - the executable file is called namd2.
+NAMD 2.14 was built with GCC 11.2.0 (CPE 21.09); it uses Cray MICH 8.1.9, Cray FFTW 3.3.8.11,
+TCL 8.5.9 and Charm++ 6.10.2.
+
+   - Installed by: M. Bareford, EPCC"
+   - Date: 13 October 2021\n"
+]])
+
+load("PrgEnv-gnu")
+load("cray-fftw")
+load("cpe/21.09")
+load("xpmem")
+load("perftools-base")
+
+local pkgName = myModuleName()
+local pkgNameVer = myModuleFullName()
+local pkgNameBase = pathJoin("/work/y07/shared/apps/core", pkgName)
+local pkgVersionBase = pathJoin("/work/y07/shared/apps/core", pkgNameVer)
+
+prepend_path("PATH", pathJoin(pkgVersionBase, "bin"))
+prepend_path("LD_LIBRARY_PATH", "/work/y07/shared/apps/core/namd/tcl/8.5.9/lib")
+
+family("namd")
