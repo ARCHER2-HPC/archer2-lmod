7a25154ec4946e10381c2104a61ac6b8d7609afd
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2021-10-14 13:13:07 +0100

    Added Eigen module

---
+++ libs/core/eigen/3.4.0.lua
@@ -0,0 +1,14 @@
+help([[
+Eigen 3.4.0
+
+Eigen is a C++ template library for linear algebra: matrices, vectors, numerical solvers, and related algorithms.
+
+Once this module has been loaded, you can include the header files located in /work/y07/shared/libs/core/eigen/3.4.0 in your programs.
+
+Maintained by C. Laoide-Kemp, EPCC
+Date: 14 October 2021
+]])
+
+prepend_path("CPATH","/work/y07/shared/libs/core/eigen/3.4.0/include")
+
+family("eigen")
