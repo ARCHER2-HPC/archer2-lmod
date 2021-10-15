f50cbbcd97af52e68eedd8b00bc518c36055ad7d
ARCHER2 CSE Service <support@archer2.ac.uk>
2021-10-15 14:34:04 +0100

    namd/2.14 is the smp version and is the default

--- apps/core/namd/2.14-nosmp.lua
+++ apps/core/namd/2.14-nosmp.lua
@@ -1,17 +1,17 @@
 help([[
-NAMD 2.14.smp
-=============
+NAMD 2.14-nosmp
+===============
 NAMD is a parallel molecular dynamics code designed for high-performance simulation of large biomolecular systems,
 see https://www.ks.uiuc.edu/Research/namd/ .
 
-This NAMD 2.14 has been built with SMP support, which introduces a dedicated communication thread for every process. 
+This NAMD 2.14 has been built without SMP support.
 
 This module sets up your environment to access NAMD 2.14 - the executable file is called namd2.
 NAMD 2.14 was built with GCC 11.2.0 (CPE 21.09); it uses Cray MICH 8.1.9 (OFI), Cray FFTW 3.3.8.11,
 TCL 8.5.9 and Charm++ 6.10.2.
 
    - Installed by: M. Bareford, EPCC"
-   - Date: 13 October 2021\n"
+   - Date: 15 October 2021\n"
 ]])
 
 always_load("PrgEnv-gnu")
