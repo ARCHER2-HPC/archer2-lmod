793e7e6c6e584ec122d4402834939983f44b58c6
ARCHER2 CSE Service <support@archer2.ac.uk>
2022-05-26 18:54:11 +0100

    Finishes Darshan reorganisagtion

---
+++ utils/compiler/crayclang/10.0/darshan/3.3.1.lua
@@ -0,0 +1,20 @@
+help([[
+Darshan Runtime
+==========
+
+This module provides access to the runtime for the
+Darshan IO monitoring tool.
+
+For details of Darshan on ARCHER2 see:
+https://docs.archer2.ac.uk/tools/darshan/
+]])
+
+local darshan_root = "/work/y07/shared/utils/compiler/crayclang/10.0/darshan-runtime"
+
+setenv("DARSHAN_ROOT", darshan_root)
+setenv("DARSHAN_DIR", darshan_root)
+
+setenv("SRUN_EXPORT_ENV","ALL,LD_PRELOAD=" .. darshan_root .. "/lib/libdarshan.so")
+
+family("darshan")
+
