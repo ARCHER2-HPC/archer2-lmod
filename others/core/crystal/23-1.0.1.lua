6475e95e3cc10d15e9f65e2cc47b5a2bed7fad62
ARCHER2 CSE Service <support@archer2.ac.uk>
2023-01-26 13:43:15 +0000

    Adds CRYSTAL 23 module

---
+++ others/core/crystal/23-1.0.1.lua
@@ -0,0 +1,21 @@
+help([[
+CRYSTAL23 1.0.1
+===============
+
+This module sets up your environment to
+access CRYSTAL23 1.0.1 using CPE/21.09
+
+        Installed: 25 January 2023
+        Installed by: B.G.Searle, STFC Daresbury
+]])
+
+load("PrgEnv-gnu")
+load("cpe/21.09")
+prepend_path("LD_LIBRARY_PATH", os.getenv("CRAY_LD_LIBRARY_PATH"))
+
+local pkgNameVer = myModuleFullName()
+local base = pathJoin("/work/y07/shared/apps/core", pkgNameVer)
+
+prepend_path("PATH", pathJoin(base, "bin"))
+ 
+family("crystal")
