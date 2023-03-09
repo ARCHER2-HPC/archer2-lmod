24a80141019a8572856a13f1163955edbf6b5a3f
ARCHER2 CSE Service <support@archer2.ac.uk>
2023-03-09 11:12:18 +0000

    Updates VASP and LAMMPS modules

--- apps/core/vasp/6/6.3.2-vtst.lua
+++ apps/core/vasp/6/6.3.2-vtst.lua
@@ -1,12 +1,11 @@
 help([[
-VASP 6.3.0
+VASP 6.3.2
 ==========
 
-This module sets up your environment to access VASP 6.3.0. This
-version has been compiled against the AOCL 3.1 numerical performance
-libraries from AMD.
+This module sets up your environment to access VASP 6.3.2 with
+the VASP Transition State Tools from the University of Texas included.
 
-Once this module has been loaded you can access the VASP 6.3.0
+Once this module has been loaded you can access the VASP 6.3.1
 executables as:
 
  *'vasp_std'       - Multiple k-point (complex) version
@@ -17,12 +16,31 @@ If you are only interested in the gamma-point, the
 gamma-point version of the code runs about 30-50%
 faster than the complex code.
 
+The VTST scripts are available in the $VTSTSCRIPTS directory.
+
 For access to VASP, please request access through
 SAFE. see:
 
   * https://epcced.github.io/safe-docs/safe-for-users/#how-to-request-access-to-a-package-group
 
-   Compiled: 19 February 2022"
+Modules loaded at compile time:
+
+  1) gcc/10.2.0
+  2) craype/2.7.6
+  3) craype-x86-rome
+  4) perftools-base/21.02.0
+  5) xpmem/2.2.40-7.0.1.0_2.7__g1d7a24d.shasta
+  6) bolt/0.7
+  7) epcc-setup-env
+  8) load-epcc-module
+  9) PrgEnv-gnu/8.0.0
+ 10) cray-libsci/21.08.1.2
+ 11) cray-fftw/3.3.8.13
+ 12) cray-ucx/2.7.0-1
+ 13) craype-network-ucx
+ 14) cray-mpich-ucx/8.1.9
+
+   Compiled: 9 February 2023"
    Maintained by: A. R. Turner, EPCC"
 ]])
 
@@ -32,18 +50,20 @@ local pkgNameBase = pathJoin("/work/y07/shared/apps/core", pkgName)
 local pkgVersionBase = pathJoin("/work/y07/shared/apps/core", pkgNameVer)
 
 load("PrgEnv-gnu")
-load("cpe/21.09")
-unload("cray-libsci")
-load("craype-network-ucx")
-load("cray-mpich-ucx")
+load("cray-libsci/21.08.1.2")
+load("cray-fftw/3.3.8.13")
+load("cray-hdf5-parallel/1.12.1.1")
 prepend_path("LD_LIBRARY_PATH", os.getenv("CRAY_LD_LIBRARY_PATH"))
-prepend_path("LD_LIBRARY_PATH", pathJoin("/work/y07/shared/apps/core/vasp/6/6.3.0/aocl-3.1-gcc10", "lib"))
+load("craype-network-ucx")
+load("cray-mpich-ucx/8.1.9")
 setenv("UCX_IB_REG_METHODS", "direct")
 
 prepend_path("PATH", pathJoin(pkgVersionBase, "bin"))
+prepend_path("PATH", pathJoin(pkgVersionBase, "vtstscripts-1029"))
 setenv("VASP", pkgVersionBase)
 
-setenv("VASP_VDW_KERNEL", pathJoin(pkgNameBase, "6/vdw_kernel"))
+setenv("VASP_VDW_KERNEL", pathJoin(pkgVersionBase, "vdw_kernel"))
+setenv("VTSTSCRIPTS", pathJoin(pkgVersionBase, "vtstscripts-1029"))
 setenv("VASP_PSPOT_DIR", pathJoin(pkgNameBase, "6/potpaw"))
 
 family("vasp")
