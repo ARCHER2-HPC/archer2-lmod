5ae8258734acc435ceb8a96bca4d246640fbe5f9
ARCHER2 CSE Service <support@archer2.ac.uk>
2023-03-09 16:40:31 +0000

    Small changes to help description of LAMMPS/17_Feb_2023 module

--- apps/core/lammps/17_Feb_2023.lua
+++ apps/core/lammps/17_Feb_2023.lua
@@ -3,10 +3,10 @@ LAMMPS 23 June 2022 Update 3 version
 ====================================
 
 This module defines the system paths and environment variables
-needed to use the stable build of LAMMPS v. 23 June 2022 Update 2. 
+needed to use the stable build of LAMMPS v. 23 June 2022 Update 3. 
 Once the module is loaded the executable will be available as 'lmp'.
 
-  Installed by: R. Apóstolo EPCC
+  Installed by: R. Apóstolo, EPCC
   Date: 9 March 2023
 ]])
 
