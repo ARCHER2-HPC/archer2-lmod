4b1fd6a440740e8ad34d54b12227c3ed68f14cb6
ARCHER2 CSE Service <support@archer2.ac.uk>
2023-03-31 11:42:44 +0100

    Updated LAMMPS modules

--- apps/core/lammps/17Feb2023.lua
+++ apps/core/lammps/17Feb2023.lua
@@ -1,17 +1,18 @@
 help([[
-LAMMPS 23 June 2022 Update 2 version
+LAMMPS 23 June 2022 Update 3 version
 ====================================
 
 This module defines the system paths and environment variables
-needed to use the stable build of LAMMPS v. 23 June 2022 Update 2. 
+needed to use the stable build of LAMMPS v. 23 June 2022 Update 3. 
 Once the module is loaded the executable will be available as 'lmp'.
 
-  Installed by: J. Sindt, EPCC
-  Date: 1 February 2023
+  Installed by: R. Apóstolo, EPCC
+  Date: 27 March 2023
 ]])
 
-load("PrgEnv-gnu")
-load("cpe/21.09")
+load("cpe/22.12")
+load("cray-fftw/3.3.10.3")
+
 prepend_path("LD_LIBRARY_PATH", os.getenv("CRAY_LD_LIBRARY_PATH"))
 
 pushenv("SLURM_CPU_FREQ_REQ","2250000")
