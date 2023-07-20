991a36284b27861f619608511b89ca83b3eff50e
ARCHER2 CSE Service <support@archer2.ac.uk>
2023-07-20 09:40:20 +0100

    Adds amd-uprof

--- utils/core/amd-uprof/4.0.341.lua
+++ utils/core/amd-uprof/4.0.341.lua
@@ -1,22 +1,22 @@
 help([[
-AMD uProf 3.6.449
+AMD uProf 4.0.341
 =================
 AMD uProf (pronounced MICRO-prof) is a software profiling analysis tool for x86 applications.
 It provides event information unique to the AMD "Zen"-based processors and AMD INSTINCT MI Series accelerators.
 
 See https://developer.amd.com/amd-uprof/ .
 
-   - Installed by: M. Bareford, EPCC"
-   - Date: 7 November 2022\n"
+   - Installed by: A. Turner, EPCC"
+   - Date: 14 June 2022\n"
 ]])
 
 local pkgName = myModuleName()
 local pkgNameVer = myModuleFullName()
-local pkgNameBase = pathJoin("/work/y07/shared/utils/dev", pkgName)
-local pkgVersionBase = pathJoin("/work/y07/shared/utils/dev", pkgNameVer)
+local pkgNameBase = pathJoin("/work/y07/shared/utils/core", pkgName)
+local pkgVersionBase = pathJoin("/work/y07/shared/utils/core", pkgNameVer)
 
 prepend_path("PATH", pathJoin(pkgVersionBase, "bin"))
 prepend_path("CPATH", pathJoin(pkgVersionBase, "include"))
 prepend_path("LD_LIBRARY_PATH", pathJoin(pkgVersionBase, "bin"))
 
-family("amd-uprof")
+family("amduprof")
