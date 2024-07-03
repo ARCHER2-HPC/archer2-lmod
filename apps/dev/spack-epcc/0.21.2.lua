5d9a81af6670aa70da5d424f9a5d273020d72834
ARCHER2 CSE Service <support@archer2.ac.uk>
2024-07-03 14:30:21 +0100

    Spack modules set user cache location and automatically source setup script.

--- apps/dev/spack-epcc/0.21.2.lua
+++ apps/dev/spack-epcc/0.21.2.lua
@@ -1,7 +1,12 @@
 help([[
 Spack 0.21.2
 ==============
-Spack is a package management tool for hpc.
+Spack is a package management tool for HPC.
+This module enables Spack and configures for
+software packages to be installed into the
+central CSE software directories, and as such
+is intended for use only by members of the
+ARCHER2 CSE team.
 
 - Installed by: L. Parisi, EPCC"
    - Date: 28 March 2024\n"
@@ -11,4 +16,16 @@ local pkgBase = "/mnt/lustre/a2fs-nvme/work/y07/shared/spack/0.21.2/spack-epcc"
 
 setenv("SPACK_ROOT_EPCC", pkgBase)
 setenv("SPACK_ROOT",pathJoin(pkgBase,"spack-0.21.2"))
+
+-- This is the ARCHER2 CSE module for use by EPCC staff. This means that software
+-- will be installed into a .spack directory located in the /work/y07/shared
+-- central software directories.
+setenv("SPACK_USER_CACHE_PATH", pathJoin(pkgBase, ".spack"))
+
+-- NB this command sources the Spack setup script required for shell completion
+-- and also for commands such as `spack load`. Using source_sh() would be
+-- preferable but this misses *something* that the script does when converting
+-- it to Lmod commands, so we run execute{} instead.
+execute{cmd=". ${SPACK_ROOT}/share/spack/setup-env.sh", modeA={"load"}}
+
 family("spack")
--- apps/dev/spack/0.21.2.lua
+++ apps/dev/spack/0.21.2.lua
@@ -1,7 +1,11 @@
 help([[
 Spack 0.21.2
 ==============
-Spack is a package management tool for hpc.
+Spack is a package management tool for HPC.
+This module enables Spack and configures for
+software packages to be installed into the
+user's directories at
+/work/group/group/username/.spack.
 
 - Installed by: L. Parisi, EPCC"
    - Date: 28 March 2024\n"
@@ -11,4 +15,17 @@ local pkgBase = "/mnt/lustre/a2fs-nvme/work/y07/shared/spack/0.21.2/spack"
 
 setenv("SPACK_ROOT_EPCC", pkgBase)
 setenv("SPACK_ROOT",pathJoin(pkgBase,"spack-0.21.2"))
+
+-- This is the user level package which will install packages to a directory
+-- called .spack located in the user's /work directory.
+local user = subprocess("id -un")
+local group = subprocess("id -gn")
+setenv("SPACK_USER_CACHE_PATH", pathJoin("/work", group, group, user, ".spack"))
+
+-- NB this command sources the Spack setup script required for shell completion
+-- and also for commands such as `spack load`. Using source_sh() would be
+-- preferable but this misses *something* that the script does when converting
+-- it to Lmod commands, so we run execute{} instead.
+execute{cmd=". ${SPACK_ROOT}/share/spack/setup-env.sh", modeA={"load"}}
+
 family("spack")
