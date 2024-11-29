fadd6d7cf32ad64d923750470129555de7e4a0f5
ARCHER2 CSE Service <support@archer2.ac.uk>
2024-11-29 22:07:44 +0000

    Added SCons 4.8.1 and removed versions 4.3.0 and 4.7.0.

--- python/core/scons/4.8.1.lua
+++ python/core/scons/4.8.1.lua
@@ -6,18 +6,20 @@ SCons is a modern software construction tool - a software utility for building s
 
 https://pypi.org/project/SCons/
 
-Build instructions: https://github.com/hpc-uk/build-instructions/blob/main/pyenvs/scons/build_scons_4.3.0_archer2.md
+Build instructions: https://github.com/hpc-uk/build-instructions/blob/main/pyenvs/scons/build_scons_4.8.1_archer2.md
 
-  Installed by: W. Lucas, EPCC
-  Date: 8 March 2024
+  Installed by: M. Bareford, EPCC
+  Date: 29 November 2024
 ]])
 
 always_load("cray-python/3.9.13.1")
 
+local modBase = os.getenv("EPCC_SOFTWARE_DIR")
+
 local pkgNameVer = myModuleFullName()
-local pkgVersionBase = pathJoin("/work/y07/shared/python/core", pkgNameVer)
+local pkgVersionBase = pathJoin(modBase, "python/core", pkgNameVer)
 local pythonSitePackages = pathJoin(pkgVersionBase, "python/3.9.13.1/lib/python3.9/site-packages")
- 
+
 prepend_path("PATH", pathJoin(pkgVersionBase, "python/3.9.13.1/bin"))
 prepend_path("PYTHONPATH", pythonSitePackages)
 prepend_path("MANPATH", pythonSitePackages)
