e237a9e4ceca32ca9441040af414d875f9fc8f40
ARCHER2 CSE Service <support@archer2.ac.uk>
2024-12-04 14:30:59 +0000

    Bring repo up to date

--- apps/core/quantum_espresso/7.3.1.lua
+++ apps/core/quantum_espresso/7.3.1.lua
@@ -1,23 +1,26 @@
 help([[
 Quantum Esspresso 7.3.1
-=====================
+=======================
 
 This module sets up your environment to access Quantum Esspresso
-6.8. Once loaded you can access MPI-enabled Quantum Esspresso
+7.3.1. Once loaded you can access MPI-enabled Quantum Esspresso
 executables using the usual commands.
 
    Installed by: E. Broadway, EPCC
    Installation date: 26th June 2024
 ]])
 
-load ("PrgEnv-gnu")
-
-prepend_path("LD_LIBRARY_PATH", os.getenv("CRAY_LD_LIBRARY_PATH"))
+local modbase = os.getenv("EPCC_SOFTWARE_DIR") or "/work/y07/shared"
+local base = pathJoin(modbase, "apps/core")
 
 local pkgName = myModuleName()
 local pkgNameVer = myModuleFullName()
-local pkgNameBase = pathJoin("/work/y07/shared/apps/core", pkgName)
-local pkgVersionBase = pathJoin("/work/y07/shared/apps/core", pkgNameVer)
+local pkgNameBase = pathJoin(base, pkgName)
+local pkgVersionBase = pathJoin(base, pkgNameVer)
+
+load("PrgEnv-gnu")
+load("cray-fftw")
+load("cray-hdf5-parallel") 
 
 prepend_path("PATH", pathJoin(pkgVersionBase, "bin"))
 family("quantum_espresso")
